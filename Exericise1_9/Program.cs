04971283e5dcf66b7ada169f9ac97aba7ad62635
Eleftheria <eleftheria.nt98@gmail.com>
2024-11-29 15:13:55 +0200

    291124-1513

---
+++ Exericise1_9/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Ex1_9
+{
+    public class Book
+    {
+        public virtual int GetPageCount()
+        {
+            return 1; 
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book baseBook = new Book();
+            Book eragonBook = new Eragon();
+            Book gTBook = new GameTheory();
+            
+            Console.WriteLine($"Book number: {baseBook.GetPageCount()}, Eragon number: {eragonBook.GetPageCount()}, GameTheory number: {gTBook.GetPageCount()} ");
+        }
+    }
+
+    public class Eragon : Book
+    {
+        public override int GetPageCount()
+        {
+            return base.GetPageCount() + 6;
+        }
+    }
+
+    public class GameTheory : Book
+    {
+        public override int GetPageCount()
+        {
+            return base.GetPageCount() + 10;
+        }
+    }
+    
+}
