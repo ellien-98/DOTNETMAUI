128a27d4b95b86ec3bb2414077e4b378aa921e68
Eleftheria <eleftheria.nt98@gmail.com>
2024-12-02 15:02:40 +0200

    2comm

--- Exercise1_12/Program.cs
+++ Exercise1_12/Program.cs
@@ -7,25 +7,24 @@ namespace Exercise1_12
         static void Main(string[] args)
         {
 
-			//string[] menuOptions = {"Potatoes with Cheddar", "Mushroom Risotto", "Pizza", "Press 0 to exit" };
-			ArrayList menuOptions = new ArrayList() {"Potatoes with Cheddar", "Mushroom Risotto", "Pizza", "Press 0 to exit" };
-            Console.WriteLine($"Please select your dish");
-			for (int i = 0; i < menuOptions.Count; i++)
+			string[] menuOptions = {"Potatoes with Cheddar", "Mushroom Risotto", "Pizza", "Press 0 to exit" };
+			//ArrayList menuOptions = new ArrayList() {"Potatoes with Cheddar", "Mushroom Risotto", "Pizza", "Press 0 to exit" };
+            Console.WriteLine($"Please select your dish:");
+			for (int i = 0; i < menuOptions.Length; i++)
         	{
-            	Console.WriteLine($"{i + 1} --> {menuOptions[i]}");
+            	Console.WriteLine($"Option {i + 1} --> {menuOptions[i]}");
         	}
-			// Console.WriteLine("Please select a food option:");
 			string menuChoice = Console.ReadLine();
 			switch (menuChoice)
     		{
         		case "1":
-            		Console.WriteLine($"Your selection: {menuOptions[0]}");
+            		Console.WriteLine($"Your selection is: {menuOptions[0]}");
             		break;
         		case "2":
-            		Console.WriteLine($"Your selection: {menuOptions[1]}");
+            		Console.WriteLine($"Your selection is: {menuOptions[1]}");
             		break;
         		case "3":
-            		Console.WriteLine($"Your selection: {menuOptions[2]}");
+            		Console.WriteLine($"Your selection is: {menuOptions[2]}");
             		break;
         		case "0":
             		Console.WriteLine("Exiting the program.");
