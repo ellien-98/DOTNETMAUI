23161b03b6b5285560cf7c6607c7344ded028915
Eleftheria <eleftheria.nt98@gmail.com>
2024-12-02 14:48:56 +0200

    fcomm

--- Exercise1_12/Program.cs
+++ Exercise1_12/Program.cs
@@ -1,11 +1,39 @@
-﻿namespace Exercise1_12
+﻿using System.Collections;
+
+namespace Exercise1_12
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int aInt = 0;
-            Console.WriteLine($"Book number: {aInt} ");
+
+			//string[] menuOptions = {"Potatoes with Cheddar", "Mushroom Risotto", "Pizza", "Press 0 to exit" };
+			ArrayList menuOptions = new ArrayList() {"Potatoes with Cheddar", "Mushroom Risotto", "Pizza", "Press 0 to exit" };
+            Console.WriteLine($"Please select your dish");
+			for (int i = 0; i < menuOptions.Count; i++)
+        	{
+            	Console.WriteLine($"{i + 1} --> {menuOptions[i]}");
+        	}
+			// Console.WriteLine("Please select a food option:");
+			string menuChoice = Console.ReadLine();
+			switch (menuChoice)
+    		{
+        		case "1":
+            		Console.WriteLine($"Your selection: {menuOptions[0]}");
+            		break;
+        		case "2":
+            		Console.WriteLine($"Your selection: {menuOptions[1]}");
+            		break;
+        		case "3":
+            		Console.WriteLine($"Your selection: {menuOptions[2]}");
+            		break;
+        		case "0":
+            		Console.WriteLine("Exiting the program.");
+            		break;
+        		default:
+            		Console.WriteLine("Wrong selection!");
+            		break;
+    		}
         }
     }
 }
